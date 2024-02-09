abcc98d382b5943e499a83e50e482d4e1665dfb3
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-02-09 15:54:10 +0530

    change dir

--- backend/api/index.js
+++ backend/api/index.js
@@ -17,7 +17,7 @@ app.get("/api/item/:slug", (req, res) => {
   const { slug } = req.params;
   res.end(`Item: ${slug}`);
 });
-
-app.use(express.static(path.join(__dirname, "../public")));
+console.log(path.join(__dirname, "../public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 export default app;
